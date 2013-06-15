00d7fd9abfc327df32e00261aee2f9c909369958
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-06-14 17:46:10 -0700

    Bias-variance tradeoff

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -2,7 +2,7 @@
 Small helpers for code that is not shown in the notebooks
 """
 
-from sklearn import neighbors, datasets
+from sklearn import neighbors, datasets, linear_model
 import pylab as pl
 import numpy as np
 from matplotlib.colors import ListedColormap
@@ -36,4 +36,40 @@ def plot_iris_knn():
     pl.xlabel('sepal length (cm)')
     pl.ylabel('sepal width (cm)')
     pl.axis('tight')
+
+
+def plot_polynomial_regression():
+    rng = np.random.RandomState(0)
+    x = 2*rng.rand(100) - 1
+
+    f = lambda t: 1.2 * t**2 + .1 * t**3 - .4 * t **5 - .5 * t ** 9
+    y = f(x) + .4 * rng.normal(size=100)
+
+    x_test = np.linspace(-1, 1, 100)
+
+    pl.figure()
+    pl.scatter(x, y, s=4)
+
+    X = np.array([x**i for i in range(5)]).T
+    X_test = np.array([x_test**i for i in range(5)]).T
+    regr = linear_model.LinearRegression()
+    regr.fit(X, y)
+    pl.plot(x_test, regr.predict(X_test), label='4th order')
+
+    X = np.array([x**i for i in range(10)]).T
+    X_test = np.array([x_test**i for i in range(10)]).T
+    regr = linear_model.LinearRegression()
+    regr.fit(X, y)
+    pl.plot(x_test, regr.predict(X_test), label='9th order')
+
+    pl.legend(loc='best')
+    pl.axis('tight')
+    pl.title('Fitting a 4th and a 9th order polynomial')
+
+    pl.figure()
+    pl.scatter(x, y, s=4)
+    pl.plot(x_test, f(x_test), label="truth")
+    pl.axis('tight')
+    pl.title('Ground truth (9th order polynomial)')
+
 
