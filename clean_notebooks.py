d3083a190c50f61346b93b7e3427d80cbb856e0c
Olivier Grisel <olivier.grisel@ensta.org>
2013-06-24 13:58:11 -0500

    Extend the notebook helper script to record cell evaluation output for static publishing with nbviewer

--- clean_notebooks.py
+++ clean_notebooks.py
@@ -1,8 +1,14 @@
-"""Utility script to be used to cleanup the notebooks before git commit"""
+"""Utility script to be used to cleanup the notebooks before git commit
+
+This a mix from @minrk's various gists.
+
+"""
 
 import sys
 import os
 import io
+from Queue import Empty
+
 from IPython.nbformat import current
 try:
     from IPython.kernel import KernelManager
@@ -23,12 +29,75 @@ def remove_outputs(nb):
                     del cell['prompt_number']
 
 
+def run_cell(shell, iopub, cell, timeout=300):
+    # print cell.input
+    shell.execute(cell.input)
+    # wait for finish, maximum 5min by default
+    reply = shell.get_msg(timeout=timeout)['content']
+    if reply['status'] == 'error':
+        failed = True
+        print "\nFAILURE:"
+        print cell.input
+        print '-----'
+        print "raised:"
+        print '\n'.join(reply['traceback'])
+    else:
+        failed = False
+
+    # Collect the outputs of the cell execution
+    outs = []
+    while True:
+        try:
+            msg = iopub.get_msg(timeout=0.2)
+        except Empty:
+            break
+        msg_type = msg['msg_type']
+        if msg_type in ('status', 'pyin'):
+            continue
+        elif msg_type == 'clear_output':
+            outs = []
+            continue
+
+        content = msg['content']
+        # print msg_type, content
+        out = current.NotebookNode(output_type=msg_type)
+
+        if msg_type == 'stream':
+            out.stream = content['name']
+            out.text = content['data']
+        elif msg_type in ('display_data', 'pyout'):
+            for mime, data in content['data'].iteritems():
+                attr = mime.split('/')[-1].lower()
+                # this gets most right, but fix svg+html, plain
+                attr = attr.replace('+xml', '').replace('plain', 'text')
+                setattr(out, attr, data)
+            if msg_type == 'pyout':
+                out.prompt_number = content['execution_count']
+        elif msg_type == 'pyerr':
+            out.ename = content['ename']
+            out.evalue = content['evalue']
+            out.traceback = content['traceback']
+        else:
+            print "unhandled iopub msg:", msg_type
+
+        outs.append(out)
+    return outs, failed
+
+
 def run_notebook(nb):
     km = KernelManager()
     km.start_kernel(stderr=open(os.devnull, 'w'))
-    kc = km.client() if hasattr(km, 'client') else km
-    kc.start_channels()
+    if hasattr(km, 'client'):
+        kc = km.client()
+        kc.start_channels()
+        iopub = kc.iopub_channel
+    else:
+        # IPython 0.13 compat
+        kc = km
+        kc.start_channels()
+        iopub = kc.sub_channel
     shell = kc.shell_channel
+
     # simple ping:
     shell.execute("pass")
     shell.get_msg()
@@ -39,16 +108,11 @@ def run_notebook(nb):
         for cell in ws.cells:
             if cell.cell_type != 'code':
                 continue
-            shell.execute(cell.input)
-            # wait for finish, maximum 5min
-            reply = shell.get_msg(timeout=300)['content']
-            if reply['status'] == 'error':
-                failures += 1
-                print "\nFAILURE:"
-                print cell.input
-                print '-----'
-                print "raised:"
-                print '\n'.join(reply['traceback'])
+
+            outputs, failed = run_cell(shell, iopub, cell)
+            cell.outputs = outputs
+            cell['prompt_number'] = cells
+            failures += failed
             cells += 1
             sys.stdout.write('.')
 
@@ -62,16 +126,22 @@ def run_notebook(nb):
     del km
 
 
-def clean_notebook_file(fname, do_check=False):
-    print("Removing outputs for: " + fname)
+def process_notebook_file(fname, action='clean'):
+    print("Performing '{}' on: {}".format(action, fname))
     orig_wd = os.getcwd()
     with io.open(fname, 'rb') as f:
         nb = current.read(f, 'json')
 
-    if do_check:
+    if action == 'check':
         os.chdir(os.path.dirname(fname))
         run_notebook(nb)
-    remove_outputs(nb)
+        remove_outputs(nb)
+    elif action == 'collect':
+        os.chdir(os.path.dirname(fname))
+        run_notebook(nb)
+    else:
+        # Clean by default
+        remove_outputs(nb)
 
     os.chdir(orig_wd)
     with io.open(fname, 'wb') as f:
@@ -79,9 +149,11 @@ def clean_notebook_file(fname, do_check=False):
 
 
 if __name__ == '__main__':
+    # TODO: use argparse instead
     args = sys.argv[1:]
     targets = [t for t in args if not t.startswith('--')]
-    do_check = '--check' in args
+    action = 'check' if '--check' in args else 'clean'
+    action = 'collect' if '--collect-output' in args else action
     if not targets:
         targets = [os.path.join(os.path.dirname(__file__), 'notebooks')]
 
@@ -93,4 +165,4 @@ if __name__ == '__main__':
         else:
             fnames = [target]
         for fname in fnames:
-            clean_notebook_file(fname, do_check=do_check)
+            process_notebook_file(fname, action=action)
